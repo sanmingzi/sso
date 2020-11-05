54717671cf29ecda42ba5a31cd8d7dda3efba58a
sanmingzi <zhiming.zhong@outlook.com>
2020-11-05 20:14:50 +0800

    optimization of form view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def flash_class(level)
     case level
-      when :notice, 'notice' then "alert alert-info"
-      when :success, 'sucess' then "alert alert-success"
-      when :error, 'error' then "alert alert-danger"
-      when :alert, 'alert' then "alert alert-danger"
+      when :notice, 'notice' then "alert-info"
+      when :success, 'sucess' then "alert-success"
+      when :error, 'error' then "alert-danger"
+      when :alert, 'alert' then "alert-danger"
     end
   end
 end
