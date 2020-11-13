146e24cf74cc0ca05e3443f23ff604a0a5e2c123
sanmingzi <zhiming.zhong@outlook.com>
2020-11-13 17:06:03 +0800

    refactor form

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -1,14 +1,17 @@
 module FormHelper
-  def form_group_for(form, model, field, opts={}, &block)
-    value         = model[field]
-    is_invalid    = model.errors[field].present?
-    error_message = model.errors[field].try(:first)
+  def form_group_for(form, field, opts={}, &block)
+    object = form.object
+
+    value         = object[field]
+    is_invalid    = object.errors[field].present?
+    error_message = object.errors[field].try(:first)
 
     label       = opts.fetch(:label) { true }
+    label_text  = opts.fetch(:label_text) { nil }
     placeholder = opts.fetch(:placeholder) { '' }
 
     content_tag :div, class: "form-group" do
-      concat form.label(field, class: 'control-label') if label
+      concat form.label(field, label_text, class: 'control-label') if label
       if block_given?
         yield
       else
@@ -18,27 +21,3 @@ module FormHelper
     end
   end
 end
-
-# <div class="form-group <%= 'has_error' if role.errors[:name].any? %>">
-#     <label>Role name</label>
-#     <%= f.text_field :name, value: role.name, class: 'form-control is-invalid', placeholder: 'Role name' %>
-#     <%= content_tag(:div, role.errors[:name].try(:first), class: 'is-invalid') %>
-#   </div>
-#   <%= f.submit 'Create', class: 'btn btn-primary' %>
-
-#   module FormHelper
-#     def errors_for(form, field)
-#         content_tag(:p, form.object.errors[field].try(:first), class: 'help-block')
-#     end
-
-#     def form_group_for(form, field, opts={}, &block)
-#         label = opts.fetch(:label) { true }
-#         has_errors = form.object.errors[field].present?
-
-#         content_tag :div, class: "form-group #{'has-error' if has_errors}" do
-#             concat form.label(field, class: 'control-label') if label
-#             concat capture(&block)
-#             concat errors_for(form, field)
-#         end
-#     end
-# end
