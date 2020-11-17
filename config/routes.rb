f9c726a37e3481233443e0ff454ddb71c060bde4
root <root@LAPTOP-A4TOLHFB.localdomain>
2020-11-17 08:49:19 +0000

    add actions#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 
   resources :users do
     get 'search', on: :collection
@@ -10,8 +13,6 @@ Rails.application.routes.draw do
   resources :roles, only: [:new, :create, :index]
 
   resources :sessions, only: [:new, :create, :destroy]
-
-  get 'signup', to: 'users#new', as: 'signup'
-  get 'login', to: 'sessions#new', as: 'login'
-  get 'logout', to: 'sessions#destroy', as: 'logout'
+  
+  resources :actions, only: [:index]
 end
