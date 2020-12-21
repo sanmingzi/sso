6fb4be7d53ab99363a32ba9c45639969c04f912a
sanmingzi <zhiming.zhong@outlook.com>
2020-12-21 17:30:28 +0800

    add case_sensitive when validates uniqueness in models#user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
   attr_accessor :password
 
   EMAIL_REGEX = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i
-  validates :username, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true, format: EMAIL_REGEX
+  validates :username, presence: true, uniqueness: {case_sensitive: true}
+  validates :email, presence: true, uniqueness: {case_sensitive: true}, format: EMAIL_REGEX
   validates :password, presence: true, confirmation: true, if: lambda { self.encrypted_password == nil }
 
   before_save :encrypt_password
